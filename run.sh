6f15207528f5d7331f1dff27a072b91b4a885ba6
T-Dynamos <68729523+T-Dynamos@users.noreply.github.com>
2021-07-29 09:23:39 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,53 @@
+apt update
+apt upgrade -y
+apt install wget -y
+apt install root-repo
+apt install unstable-repo
+apt install x11-repo
+apt install git -y
+apt install python -y
+apt install python-pip -y
+apt install neofetch -y
+pip install lolcat
+pip install routersploit
+cd $HOME
+git clone https://github.com/Ignitetch/AdvPhishing
+git clone https://github.com/T-Dynamos/Maths_Bot m_bot
+git clone https://github.com/rajkumardusad/IP-Tracer.git ip
+git clone https://github.com/DarkSecDevelopers/HiddenEye-Legacy server
+cd $PREFIX/bin
+wget https://raw.githubusercontent.com/T-Dynamos/termux-pro/main/mbot
+wget https://raw.githubusercontent.com/T-Dynamos/Tvirus2.0/main/hack
+wget https://raw.githubusercontent.com/T-Dynamos/termux-pro/main/exploit
+wget https://raw.githubusercontent.com/T-Dynamos/termux-pro/main/msf
+wget https://raw.githubusercontent.com/T-Dynamos/termux-pro/main/rsf
+chmod +x *
+cd $HOME 
+apt install ruby -y
+gem install lolcat -y 
+apt install toilet -y
+pip install pgrep pyngrok tqdm
+cd AdvPhishing
+rm -rf secnhack
+wget https://raw.githubusercontent.com/T-Dynamos/advphishing/main/secnhack
+bash Linux-Setup.sh
+cd
+chmod +x *
+pip install lolcat 
+pip install colorama
+pip install table-ex
+pip install py-loader
+pip install requests
+cd
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip 
+unzip ngrok-stable-linux-arm.zip 
+rm ngrok-stable-linux-arm.zip 
+apt install toilet -y
+clear
+cd /data/data/com.termux/files/usr/etc
+rm profile
+wget https://raw.githubusercontent.com/T-Dynamos/termux-pro/main/profile
+cd $HOME
+wget https://raw.githubusercontent.com/T-Dynamos/termux-pro/main/c.py
+python c.py
+rm c.py
